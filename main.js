cb2e68db94bba603355f77db893f67fbd78d61e3
David Deloso <daviddeloso2022@u.northwestern.edu>
2020-07-08 22:55:20 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector('.homepage-navbar').style.top = "0";
+  } else {
+    document.querySelector('.homepage-navbar').style.top = "-50vh";
+  }
+  prevScrollpos = currentScrollPos;
+}
+const toggleSchedule = () => {
+    document.querySelector('.schedule-sidebar').classList.toggle("schedule-active")
+    document.querySelector('.schedule-clickout').classList.toggle('clickout-active');
+}
+document.querySelector('.schedule-link').onclick = function(){
+    toggleSchedule();
+}
+
+document.querySelector('.schedule-clickout').onclick =  function(){
+    toggleSchedule();
+}
+document.querySelector('#schedule-box').onclick =  function(){
+    toggleSchedule();
+}
